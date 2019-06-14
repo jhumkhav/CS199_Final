2c9606d7db2591349fee06c1c9764316df597f16
jhumkhav <43684510+jhumkhav@users.noreply.github.com>
2019-06-14 16:53:51 -0700

    Add scripts

--- scripts/samtools.sh
+++ scripts/samtools.sh
@@ -6,15 +6,9 @@
 
 module load samtools
 
-samtools sort -@ 8 -o C1_R1.bam ERR188044_chrX.sam
-samtools sort -@ 8 -o ERR188104_chrX.bam ERR188104_chrX.sam
-samtools sort -@ 8 -o ERR188234_chrX.bam ERR188234_chrX.sam
-samtools sort -@ 8 -o ERR188245_chrX.bam ERR188245_chrX.sam
-samtools sort -@ 8 -o ERR188257_chrX.bam ERR188257_chrX.sam
-samtools sort -@ 8 -o ERR188273_chrX.bam ERR188273_chrX.sam
-samtools sort -@ 8 -o ERR188337_chrX.bam ERR188337_chrX.sam
-samtools sort -@ 8 -o ERR188383_chrX.bam ERR188383_chrX.sam
-samtools sort -@ 8 -o ERR188401_chrX.bam ERR188401_chrX.sam
-samtools sort -@ 8 -o ERR188428_chrX.bam ERR188428_chrX.sam
-samtools sort -@ 8 -o ERR188454_chrX.bam ERR188454_chrX.sam
-samtools sort -@ 8 -o ERR204916_chrX.bam ERR204916_chrX.sam
+samtools sort -@ 8 -o C1_R1.bam C1_R1.sam
+samtools sort -@ 8 -o C1_R2.bam C1_R2.sam
+samtools sort -@ 8 -o C1_R3.bam C1_R3.sam
+samtools sort -@ 8 -o C2_R1.bam C2_R1.sam
+samtools sort -@ 8 -o C2_R2.bam C2_R2.sam
+samtools sort -@ 8 -o C2_R3.bam C2_R3.sam
