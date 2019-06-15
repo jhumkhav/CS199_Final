df39877a0fe33873a8eb634f86163e590700c77a
jhumkhav <43684510+jhumkhav@users.noreply.github.com>
2019-06-14 17:02:51 -0700

    Add scripts

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -6,4 +6,4 @@
 
 module load stringtie
 
-stringtie --merge -p 8 -G chrX_data/genes/chrX.gtf -o stringtie_merged.gtf chrX_data/mergelist.txt
+stringtie --merge -p 8 -G genes.gtf -o stringtie_merged.gtf /data/users/jhumkhav/rnaseq/assemblies.txt
